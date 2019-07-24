67b2d4bb7544a025641e9debf5fae4b9d716b8dd
szpgpp <szpgpp@qq.com>
2019-07-24 15:20:41 +0800

    wpf_richtextbox

---
+++ wpf_RightTextBox1/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf_RightTextBox1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            this.btnBold.Click += (sender, e) => { this.rtbText.Selection.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Bold); };
+            this.btnItalic.Click += (sender, e) => { this.rtbText.Selection.ApplyPropertyValue(TextElement.FontStyleProperty, FontStyles.Italic); };
+            this.btnUnderline.Click += (sender, e) => { this.rtbText.Selection.ApplyPropertyValue(Underline.TextDecorationsProperty, TextDecorations.Underline); };
+            this.btnStrike.Click += (sender, e) => { this.rtbText.Selection.ApplyPropertyValue(Underline.TextDecorationsProperty, TextDecorations.Strikethrough); };
+        }
+    }
+}
